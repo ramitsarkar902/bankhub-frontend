6580a85c9ddf1147e07c541ae4e8e9a5bd670002
Ramit <ramitsarkar902@gmail.com>
2021-08-24 18:45:09 +0530

    hero section 24-08 in progress

---
+++ src/components/HeroSection/HeroElements.jsx
@@ -0,0 +1,91 @@
+import styled from "styled-components";
+import { MdKeyboardArrowRight, MdArrowForward } from "react-icons/md";
+
+export const HeroContainer = styled.div`
+  background: #0c0c0c;
+  align-items: center;
+  padding: 0 30px;
+  position: relative;
+  z-index: 1;
+  height: 800px;
+  display: flex;
+  justify-content: center;
+
+  /*Add more styles*/
+`;
+
+export const HeroBg = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+  height: 100%;
+`;
+
+export const VideoBg = styled.video`
+  width: 100%;
+  height: 100%;
+  -o-object-fit: cover;
+  object-fit: cover;
+  background: #232a34;
+`;
+
+export const HeroContent = styled.div`
+  position: absolute;
+  padding: 8px 24px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1200px;
+  z-index: 3;
+`;
+
+export const HeroContentH1 = styled.h1`
+  color: #fff;
+  font-size: 48px;
+  text-align: center;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    font-size: 32px;
+  }
+`;
+
+export const HeroContentP = styled.p`
+  color: #fff;
+  margin-top: 24px;
+  max-width: 600px;
+  text-align: center;
+  font-size: 24px;
+
+  @media only screen and (max-width: 768px) {
+    font-size: 20px;
+  }
+
+  @media only screen and (max-width: 480px) {
+    font-size: 18px;
+  }
+`;
+
+export const HeroBtnWrapper = styled.div`
+  margin-top: 32px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const ArrowForward = styled(MdArrowForward)`
+  margin-left: 8px;
+  font-size: 20px;
+`;
+
+export const ArrowRight = styled(MdKeyboardArrowRight)`
+  margin-left: 8px;
+  font-size: 20px;
+`;
---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import Video from "../../videos/video.mp4";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroContentH1,
+  HeroContentP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
+import { Button } from "../ButtonElements";
+
+const Hero = () => {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer>
+      <HeroBg>
+        <VideoBg autoPlay loop muted src={Video} type="/video/mp4" />
+      </HeroBg>
+      <HeroContent>
+        <HeroContentH1>Virtual Banking Made Easy & Simple</HeroContentH1>
+        <HeroContentP>
+          Sign Up for a new account today and receive amazing rewards on next
+          payment..
+        </HeroContentP>
+        <HeroBtnWrapper>
+          <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}>
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+};
+
+export default Hero;
