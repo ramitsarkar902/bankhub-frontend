f522c6991aeeae081fe9d93debec2a0dc905b9ed
Ramit <ramitsarkar902@gmail.com>
2021-08-26 15:13:33 +0530

    added copyright info

--- src/components/Footer/FooterElements.js
+++ src/components/Footer/FooterElements.js
@@ -54,7 +54,7 @@ export const FooterLinkTitle = styled.h1`
     margin-bottom:16px;
 
 `;
-
+//use styled.a for href
 export const FooterLink = styled(Link)`
   color: #fff;
   text-decoration: none;
@@ -64,5 +64,55 @@ export const FooterLink = styled(Link)`
   $:hover {
     color: #01bf71;
     transition: 0.3s ease-out;
+  }
+`;
+
+export const SocialMedia = styled.div`
+  max-width: 1000px;
+  width: 100%;
+`;
+
+export const SocialMediaWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1100px;
+  margin: 40px auto 0 auto;
+
+  @media only screen and (max-width: 820px) {
+    flex-direction: column;
+  }
+`;
+
+export const SocialLogo = styled(Link)`
+  color: #fff;
+  justify-self: start;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+  margin-bottom: 16px;
+  font-weight: bold;
+`;
+
+export const WebsiteRights = styled.small`
+  color: #fff;
+  margin-bottom: 16px;
+`;
+
+export const SocialIcons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 240px;
+`;
+
+export const SocialIconLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+
+  &:hover {
+    color: #2acad6;
   }
 `;
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,7 +7,20 @@ import {
   FooterLinkItems,
   FooterLinksContainer,
   FooterLinkTitle,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialIconLink,
+  SocialIcons,
+  SocialLogo,
+  WebsiteRights,
 } from "./FooterElements";
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaLinkedinIn,
+} from "react-icons/fa";
 const Footer = () => {
   return (
     <FooterContainer>
@@ -41,11 +54,37 @@ const Footer = () => {
               <FooterLinkTitle>Contact Us</FooterLinkTitle>
               <FooterLink to="/signin">About us</FooterLink>
               <FooterLink to="/signin">Testimonials</FooterLink>
-              <FooterLink to="/signin">Investors</FooterLink>
+              <FooterLink href="https://www.facebook.com/" to="/signin">
+                Investors
+              </FooterLink>
               <FooterLink to="/signin">Terms Of Services</FooterLink>
             </FooterLinkItems>
           </FooterLinksWrapper>
         </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo to="/home">bankHUB</SocialLogo>
+            <WebsiteRights>
+              Ramit Sarkar @ {new Date().getFullYear} All rights reserved.
+            </WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink
+                href="https://www.facebook.com/"
+                target="_blank"
+                aria-label="Facebook"
+              >
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href="/" target="_blank" aria-label="Instagram">
+                <FaInstagram />
+              </SocialIconLink>
+
+              <SocialIconLink href="/" target="_blank" aria-label="Twitter">
+                <FaTwitter />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
       </FooterWrap>
     </FooterContainer>
   );
